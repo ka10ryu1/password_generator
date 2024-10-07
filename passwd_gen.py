100c502cc401f044c67684c8e45d063ac53f53c9
ka10ryu1 <lsyp.tuiovjo.1231@gmail.com>
2024-10-07 11:44:40 +0900

    ✨ パスワード生成スクリプトにファイル保存機能を追加
    ## 変更内容
    - 引数で指定したパスにパスワードを保存する機能を追加しました。
    - 保存先ディレクトリとファイル名を指定できるようになり、デフォルトでディレクトリを作成します。
    
    ## 注意点
    - 指定されたディレクトリが存在しない場合、自動的に作成されます。
    - ファイル名が指定されていない場合は、パスワードをファイルに保存しません。
    - `.txt`拡張子が自動追加されるため、ファイル名指定時は注意が必要です。

--- passwd_gen.py
+++ passwd_gen.py
@@ -3,6 +3,7 @@
 import secrets
 from argparse import ArgumentParser, Namespace
 from string import ascii_letters, digits
+from pathlib import Path
 
 from measure_time import time_it, get_file_name
 
@@ -17,11 +18,41 @@ def command():
         help='生成するパスワードの文字数 [default:%(default)s]',
     )
     parser.add_argument(
+        '--out',
+        type=Path,
+        default='out',
+        metavar='PATH',
+        help='パスワードの保存ディレクトリ [default:%(default)s]',
+    )
+    parser.add_argument(
+        '--name',
+        type=Path,
+        default=None,
+        metavar='PATH',
+        help='パスワードを保存したい時のファイル名 [defaultは作成しない]',
+    )
+    parser.add_argument(
         '--no_symbol',
         action='store_true',
         help='記号を使用しないモード',
     )
     return parser.parse_args()
+
+
+@time_it('mkdir')
+def mkdir(path: Path):
+    # 親ディレクトリが存在しない場合
+    if not path.parent.exists():
+        path.parent.mkdir(parents=True)
+    # ファイルが指定されている場合（is_file()は存在しているファイルにしか反応しない）
+    elif path.suffix:
+        pass
+    # ディレクトリが指定されている場合
+    elif not path.exists():
+        print(f'mkdir {path.as_posix()}')
+        path.mkdir()
+
+    return path
 
 
 def _select_password(length: int, get_pass_chars: str):
@@ -53,14 +84,25 @@ def main(args: Namespace):
         int: 常に0を返す
     """
     print(args)
-    print(f'{"_/" * (args.n // 2)}')
 
-    if args.no_symbol:
-        print(get_random_password_v1(args.n))
-    else:
-        print(get_random_password_v2(args.n))
+    grp = get_random_password_v1 if args.no_symbol else get_random_password_v2
+    passwd = grp(args.n)
 
-    print(f'{"_/" * (args.n // 2)}')
+    print(f'{"_/" * (args.n // 2 + 4)}')
+    print(f'{passwd=:}')
+    print(f'{"_/" * (args.n // 2 + 4)}')
+
+    if args.name is not None:
+
+        if not args.name.suffix:
+            args.name = args.name.with_suffix('.txt')
+
+        save_path = mkdir(args.out) / args.name
+        with save_path.open('w') as f:
+            f.write(passwd)
+
+        print(f'write {save_path.as_posix()}')
+
     return 0
 
 
