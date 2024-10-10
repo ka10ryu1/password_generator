6b965117eafd3b880a1487affc68ed6c97c8e22c
ka10ryu1 <lsyp.tuiovjo.1231@gmail.com>
2024-10-10 15:18:55 +0900

    🐛 ログ検索機能を改良し、複数行の処理とインデント操作を改善 ## 変更点 - `_get_exec_pos`関数を更新し、実行ブロックの検索とフォーマット処理を改善 - `_search_log`関数で実行ブロック内の行を識別し、インデント操作を制御するロジックを追加 - デバッグ用のコメントや変数を追加して、処理の追跡を容易に - 実行回数と試行結果の出力を追加し、繰り返し処理の結果を確認できるように変更
    ## 注意点
    - インデント操作は繰り返し処理を順次実行するように設計されているため、処理回数を増やしても影響がない場合に処理が終了する
    - 入力行が変更されない場合、早期終了するが、大量データの処理では動作確認が必要

--- measure_time.py
+++ measure_time.py
@@ -62,7 +62,10 @@ def time_it(pattern: str = '', get_time: bool = False):
 
     return _timer
 
+
+### ここから先の処理は、このスクリプトが直接呼ばれた場合のみ実行される
 from argparse import ArgumentParser, Namespace
+
 
 def _command():
     parser = ArgumentParser(description='パスワード生成スクリプト')
@@ -78,49 +81,89 @@ def _command():
         default='out',
         metavar='PATH',
         help='解析したログの保存ディレクトリ [default:%(default)s]',
-    )    
+    )
     parser.add_argument(
         '--out_path',
         type=Path,
         default='log_analyzed.txt',
         metavar='PATH',
         help='解析したログのファイル名 [default:%(default)s]',
-    )    
+    )
     parser.add_argument(
         '--overwrite',
         action='store_true',
         help='入力パスをそのまま出力パスとして使用する上書きモード',
-    )    
+    )
     return parser.parse_args()
 
-def _get_exec_name(line:str):
-    return line.split('[')[-1].replace(']','')
 
-def _get_exec_pos(line:str):
-    return line.find('<')
+def _get_exec_name(line: str):
+    return line.split('[')[-1].replace(']', '')
 
-def _search_log(lines,check_exec:str = '<< exec start',space_num:int = 4):
+
+def _get_exec_pos(line: str, check_marker: str = '<'):
+    return line.find(check_marker)
+
+
+def _search_log(
+    input_lines: list[str], check_exec: str = '<< exec start', space_num: int = 4
+):
+    """
+    入力行のリストを処理して、実行ブロックを識別しフォーマットする
+
+    Args:
+        input_lines (list of str): 処理する入力行
+        check_exec (str, optional): 実行ブロックの開始を識別するマーカ文字列
+        space_num (int, optional): 実行ブロック内の行の先頭に追加するスペースの数
+
+    Returns:
+        tuple: 次の要素を含むタプル:
+        - bool: 入力行が変更されていない場合はTrue、それ以外の場合はFalse
+        - list of str: 実行ブロックがフォーマットされた処理済みの行
+    """
     old_line = ''
     continue_flag = False
     exec_name = ''
     new_lines = []
-    for line in lines:
-        # print(0,line,old_line,continue_flag,exec_name)
-        if check_exec in old_line and check_exec in line and _get_exec_pos(old_line) == _get_exec_pos(line):
+    for line in input_lines:
+        # デバッグ用
+        # print(f'{continue_flag=},{exec_name=},{_get_exec_pos(old_line):2},{_get_exec_pos(line):2}')
+        """
+        今の行と前の行が同じ実行ブロック（check_exec） かつ
+        実行ブロックの開始マーカ（check_marker）の位置が同じ かつ
+        exec_nameが空文字の場合にインデント下げを行い、exec_nameを取得し、continue_flagをセット
+
+        再度、同じexec_nameが登場した場合はインデントを戻し、exec_nameを空文字にして、continue_flagをリセット
+
+        continue_flagがTrueの場合はインデント下げを継続
+
+        それ以外の場合はそのままの行を追加
+        """
+        if (
+            check_exec in old_line
+            and check_exec in line
+            and _get_exec_pos(old_line) == _get_exec_pos(line)
+            and exec_name == ''
+        ):
+            new_lines.append(' ' * space_num + line)
             exec_name = _get_exec_name(old_line)
-            new_lines.append(' ' * space_num +line)
             continue_flag = True
+
         elif _get_exec_name(line) == exec_name:
-            continue_flag = False
             new_lines.append(line)
+            exec_name = ''
+            continue_flag = False
+
         elif continue_flag:
-            new_lines.append(' ' * space_num +line)
+            new_lines.append(' ' * space_num + line)
+
         else:
             new_lines.append(line)
 
         old_line = line
 
-    return exec_name == '',new_lines
+    return input_lines == new_lines, new_lines
+
 
 def _main(args: Namespace):
     if not args.input_log_txt.exists():
@@ -130,12 +173,24 @@ def _main(args: Namespace):
     with args.input_log_txt.open() as fp:
         lines = [line.rstrip('\n') for line in fp]
 
-    for _ in range(10):
-        flg,lines = _search_log(lines)
+    N = 10
+    for i in range(N):
+        """
+        インデント下げは一度にまとめて実施しないで、順を追って実追って実施する
+        そのため、N回繰り返しても変更がない場合は処理を終了する
+        """
+        flg, lines = _search_log(lines)
+        # デバッグ用
+        # print(f'### {i} ###')
+        # for j in lines:
+        #     print(j)
+
         if flg:
             break
-        
 
+    print(f'試行回数: {i}')
+
+    # 上書きの場合は入力パスをそのまま出力パスとして使用
     if args.overwrite:
         save_path = args.input_log_txt
     else:
@@ -151,4 +206,7 @@ def _main(args: Namespace):
 if __name__ == '__main__':
     exit(_main(_command()))
 
-logger.debug(f'### Hello {__name__.upper()} Logger! [{dt.now().strftime('%y.%m.%d %H:%M:%S')}] ###')
+### measure_time.pyが直接呼ばれていない場合は以下の処理が実行される
+logger.debug(
+    f'### Hello {__name__.upper()} Logger! [{dt.now().strftime("%y.%m.%d %H:%M:%S")}] ###'
+)
